1de948e3ba667871264acde16cd493e530209a92
HuanjiaLiu <41726668+HuanjiaLiu@users.noreply.github.com>
2021-01-10 16:24:44 -0700

    1/10/2021

---
+++ django/django_project/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = MarkdownxField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete= models.CASCADE) #delete user than delete everything
+
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+    
+    def formatted_markdown(self):
+        return markdownify(self.content)
